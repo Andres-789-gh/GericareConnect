37f420b5e7b20e6d9d21cc96b8c5eb1ccb849221
EdBudy <giovanni2003go@gmail.com>
2025-06-28 16:09:52 -0500

    styles

--- views/cuidador/html_cuidador/medicamento.php
+++ views/cuidador/html_cuidador/medicamento.php
@@ -27,7 +27,7 @@ if (isset($_GET['idEliminar'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Medicamentos</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../../css/styles.css">
 </head>
 <body>
     <div class="container">
