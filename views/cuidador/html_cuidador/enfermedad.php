099cdbfe6f5eb8890e3e135def8702edd2eef7b8
EdBudy <giovanni2003go@gmail.com>
2025-06-28 16:08:29 -0500

    styles again

--- views/cuidador/html_cuidador/enfermedad.php
+++ views/cuidador/html_cuidador/enfermedad.php
@@ -27,7 +27,7 @@ if (isset($_GET['idEliminarEnfermedad'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Enfermedades</title>
-    <link rel="stylesheet" href="<?php echo __DIR__ . '/../../../css/styles.css'; ?>">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
     <div class="container">
