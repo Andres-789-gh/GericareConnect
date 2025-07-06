2f226bc774b804f145e858449f1a91cd132ec756
EdBudy <giovanni2003go@gmail.com>
2025-07-06 07:16:21 -0500

    styles

--- views/admin/html_admin/historia_clinica.php
+++ views/admin/html_admin/historia_clinica.php
@@ -14,6 +14,7 @@ $historias_clinicas = $controller->consultarHistoriasClinicas();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GeriCare Connect | Historias Clínicas</title>
+    <link rel="stylesheet" href="../../css/styles.css">
     <link rel="stylesheet" href="../css_admin/historia_clinica_lista.css"> <!-- Se recomienda un CSS específico para la lista -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
